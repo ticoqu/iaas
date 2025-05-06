f61f1e8a72f28c0db7dd1513dcfbfd5131af1555
ticoqu <ticoquique@gmail.com>
2025-05-06 17:33:58 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "proxmox" {
     pm_tls_insecure = true
-    pm_api_url = "https://200.9.165.132:8006/api2/json"
-    pm_api_token_id = "terraform@pve!terra"
-    pm_api_token_secret = "69622fcf-f422-4551-8fd1-3214a1cd7844"
+    pm_api_url = "https://localhost:8006/api2/json"
+    pm_api_token_id = "terra@pve!te"
+    pm_api_token_secret = "69622"
 }
